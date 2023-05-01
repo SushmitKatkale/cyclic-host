4a60d6657c44ba1a6e5b4fffd127cd4bbb2064d6
Sushmit Katkale <72334943+SushmitKatkale@users.noreply.github.com>
2023-05-01 20:39:57 +0530

    main

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongosh "mongodb+srv://cluster0.layc9jh.mongodb.net/myFirstDatabase" --apiVersion 1 --username katkalesushmit');
+mongoose.connect('mongodb://localhost:27017/app');
 const db = mongoose.connection;
 
 db.on("error", console.error.bind('console','error'));
 
 db.once('open' , () => {
-    console.log("Connection established with milo_new_db");
+    console.log("Connection established with app");
 })
 
 module.exports = db;
